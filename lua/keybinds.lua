83dab640cb756a3f990e56fa1a360c8267fdcc66
CarboSauce <wilsonek2001@gmail.com>
2026-09-15 00:02:21 +0200

    Add support for dotnet and DAP
    Adds support for dotnet via easy-dotnet including formatting via
    conform-nvim aswell as DAP keybinds and ensures DAP works by running it
    on dotnet webapp

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,34 +1,28 @@
 -- General configs
-vim.api.nvim_create_user_command('Fmt', function() vim.lsp.buf.format() end, { nargs = 0 })
-vim.api.nvim_create_user_command('Impl',function() vim.lsp.buf.definition() end, { nargs = 0 })
-vim.api.nvim_create_user_command('Rename',function() vim.lsp.buf.rename() end, { nargs = 0 })
-vim.api.nvim_create_user_command('Hover',function() vim.lsp.buf.hover() end, { nargs = 0 })
-vim.api.nvim_create_user_command('Codeaction',function() vim.lsp.buf.code_action() end, { nargs = 0 })
-vim.api.nvim_create_user_command('Diag',function() vim.diagnostic.open_float() end, { nargs = 0 })
+vim.api.nvim_create_user_command(
+    'Fmt', function () require 'conform'.format({ lsp_format = 'fallback' }) end, { nargs = 0 }
+)
+vim.api.nvim_create_user_command('Impl', function () vim.lsp.buf.definition() end, { nargs = 0 })
+vim.api.nvim_create_user_command('Rename', function () vim.lsp.buf.rename() end, { nargs = 0 })
+vim.api.nvim_create_user_command('Hover', function () vim.lsp.buf.hover() end, { nargs = 0 })
+vim.api.nvim_create_user_command('Codeaction', function () vim.lsp.buf.code_action() end, { nargs = 0 })
+vim.api.nvim_create_user_command('Diag', function () vim.diagnostic.open_float() end, { nargs = 0 })
+
+vim.keymap.set('n', '<leader>qf', function () require('trouble').toggle() end, { desc = 'Quick fix' })
 
 vim.keymap.set(
-	'n',
-	'<leader>qf',
-	function() require('trouble').toggle() end,
-    { desc = 'Quick fix'}
+    'n', '<leader>tf', '<cmd>Telescope file_browser<CR>', { noremap = true, desc = 'Telescope file browser' }
 )
 
-vim.keymap.set(
-	'n',
-	'<leader>tf',
-    '<cmd>Telescope file_browser<CR>',
-	{noremap = true, desc = 'Telescope file browser'}
-)
+vim.keymap.set('n', '<leader>db', function () require 'dap'.toggle_breakpoint() end, { desc = 'DAP Toggle breakpoint' })
+vim.keymap.set('n', '<leader>dso', function () require 'dap'.step_over() end, { desc = 'DAP Step over' })
+vim.keymap.set('n', '<leader>dsi', function () require 'dap'.step_into() end, { desc = 'DAP Step into' })
+vim.keymap.set('n', '<leader>dc', function () require 'dap'.continue() end, { desc = 'DAP continue' })
 
-
-vim.keymap.set(
-    'i',
-    '<a-e>',
-    "<esc>l<cmd>lua require('nvim-autopairs.fastwrap').show()<cr>",
-    { noremap = true }
-)
+vim.keymap.set('i', '<a-e>', "<esc>l<cmd>lua require('nvim-autopairs.fastwrap').show()<cr>", { noremap = true })
 
 -- Bufdelete stuff
-vim.keymap.set('n','<leader>bd',function ()
-	require('bufdelete').bufdelete(0,false)
-end,{silent=true, noremap=true, desc = 'Bufdelete'} )
+vim.keymap.set('n', '<leader>bd', function ()
+    require('bufdelete').bufdelete(0, false)
+end, { silent = true, noremap = true, desc = 'Bufdelete' }
+)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,150 +1,157 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--depth=1",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--depth=1",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{ import = "configs" },
-	{ 'lunacookies/vim-colors-xcode' },
-	{ 'nyoom-engineering/oxocarbon.nvim', build = false },
-	{ 'RRethy/nvim-base16' },
-	{ 'LunarVim/horizon.nvim' },
-	-- plugins
-	{ 
-		"nvim-telescope/telescope-file-browser.nvim",
-		config = function()
-			require 'telescope'.load_extension 'file_browser'
-		end
-	},
-	'famiu/bufdelete.nvim',
-	{
-		'stevearc/dressing.nvim',
-		opts = {
-			winblend = 0
-		}
-	},
-	{
-		'dgagn/diagflow.nvim',
-		opts = {}
-	},
-	{
-		'Wansmer/treesj',
-		keys = {
-			{
-				"<leader>J", "<cmd>TSJToggle<cr>", desc = "Join Toggle"
-			}
-		},
-		opts = {
-			use_default_keymaps = false,
-			max_join_length = 150
-		}
-	},
-	'mfussenegger/nvim-dap',
-	{
-		'windwp/nvim-autopairs',
-		opts = {
-			fast_wrap = {
-			    map = '<esc>e',
-				chars = { '<','{', '[', '(', '"', "'" },
-				end_key = ')'
-			}
-		}
-	},
-	{
-		'nvim-telescope/telescope.nvim',
-		dependencies = {
-			'nvim-lua/plenary.nvim'
-		},
-		opts = {
-			defaults = {
-				preview = {
-					hide_on_startup = true
-				}
-			},
-			extensions = {
-				file_browser = {
-					hijack_netrw = false
-				}
-			}
-		}
-	},
-	{
-		'akinsho/toggleterm.nvim',
-		version = '*',
-		opts = {
-			float_opts = {
-				border = { "╔", "═", "╗", "║", "╝", "═", "╚", "║" }
-			},
-			open_mapping = '<c-t>',
-			direction = 'float',
-			persist_mode = false -- start always in insert mode
-		}
-	},
-	{
-		'akinsho/bufferline.nvim',
-		version = '*',
-		dependencies = {'nvim-tree/nvim-web-devicons'},
-		opts = {
-			options = {
-				mode = 'tabs'
-			}
-		}
-	},
-	{
-		'JoosepAlviste/nvim-ts-context-commentstring',
-		config = function()
-			local get_option = vim.filetype.get_option
-			vim.filetype.get_option = function(filetype, option)
-				return option == "commentstring"
-				and require("ts_context_commentstring.internal").calculate_commentstring()
-				or get_option(filetype, option)
-			end
-			vim.g.skip_ts_context_commentstring_module = true
-			require('ts_context_commentstring').setup {
-				enable_autocmd = false,
-			}
-		end,
-	},
-	{
-		'nvim-treesitter/nvim-treesitter-context',
-		opts = {
-			enable = true,
-			patterns = {
-				default = {
-					'class',
-					'function',
-					'method'
-				}
-			}
-		}
-	},
-	{
-		'windwp/nvim-ts-autotag',
-		config = function()
-			require 'nvim-ts-autotag'.setup({})
-		end
-	},
-	{
-		'stevearc/oil.nvim',
-		config = function()
-			require 'oil'.setup()
-		end
-	},
+    { import = "configs" },
+    { 'lunacookies/vim-colors-xcode' },
+    { 'nyoom-engineering/oxocarbon.nvim', build = false },
+    { 'RRethy/nvim-base16' },
+    { 'LunarVim/horizon.nvim' },
+    -- plugins
+    {
+        "nvim-telescope/telescope-file-browser.nvim",
+        config = function ()
+            require 'telescope'.load_extension 'file_browser'
+        end
+    },
+    'famiu/bufdelete.nvim',
+    {
+        'stevearc/dressing.nvim',
+        opts = {
+            winblend = 0
+        }
+    },
+    {
+        'dgagn/diagflow.nvim',
+        opts = {}
+    },
+    {
+        'Wansmer/treesj',
+        keys = {
+            {
+                "<leader>J",
+                "<cmd>TSJToggle<cr>",
+                desc = "Join Toggle"
+            }
+        },
+        opts = {
+            use_default_keymaps = false,
+            max_join_length = 150
+        }
+    },
+    'mfussenegger/nvim-dap',
+    {
+        'igorlfs/nvim-dap-view',
+        lazy = true,
+        cmd = {
+            'DapViewOpen',
+            'DapViewClose',
+            'DapViewToggle'
+        }
+    },
+    {
+        'windwp/nvim-autopairs',
+        opts = {
+            fast_wrap = {
+                map = '<esc>e',
+                chars = { '<', '{', '[', '(', '"', "'" },
+                end_key = ')'
+            }
+        }
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim'
+        },
+        opts = {
+            defaults = {
+                preview = {
+                    hide_on_startup = true
+                }
+            },
+            extensions = {
+                file_browser = {
+                    hijack_netrw = false
+                }
+            }
+        }
+    },
+    {
+        'akinsho/toggleterm.nvim',
+        version = '*',
+        opts = {
+            float_opts = {
+                border = { "╔", "═", "╗", "║", "╝", "═", "╚", "║" }
+            },
+            open_mapping = '<c-t>',
+            direction = 'float',
+            persist_mode = false -- start always in insert mode
+        }
+    },
+    {
+        'akinsho/bufferline.nvim',
+        version = '*',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        opts = {
+            options = {
+                mode = 'tabs'
+            }
+        }
+    },
+    {
+        'JoosepAlviste/nvim-ts-context-commentstring',
+        config = function ()
+            local get_option = vim.filetype.get_option
+            vim.filetype.get_option = function (filetype, option)
+                return option == "commentstring"
+                    and require("ts_context_commentstring.internal").calculate_commentstring()
+                    or get_option(filetype, option)
+            end
+            vim.g.skip_ts_context_commentstring_module = true
+            require('ts_context_commentstring').setup { enable_autocmd = false }
+        end
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        opts = {
+            enable = true,
+            patterns = {
+                default = {
+                    'class',
+                    'function',
+                    'method'
+                }
+            }
+        }
+    },
+    {
+        'windwp/nvim-ts-autotag',
+        config = function ()
+            require 'nvim-ts-autotag'.setup({})
+        end
+    },
+    {
+        'stevearc/oil.nvim',
+        config = function ()
+            require 'oil'.setup()
+        end
+    },
     {
         'MagicDuck/grug-far.nvim',
-        config = function()
-            require'grug-far'.setup({
-
-            })
+        config = function ()
+            require 'grug-far'.setup({})
         end
     },
     {
@@ -173,11 +180,12 @@ require("lazy").setup({
         }
     },
     {
-        'Civitasv/cmake-tools.nvim', opts = {}
+        'Civitasv/cmake-tools.nvim',
+        opts = {}
     }
 },
-{
-	change_detection = {
-		enabled = false
-	}
-})
+    {
+        change_detection = {
+            enabled = false
+        }
+    })
